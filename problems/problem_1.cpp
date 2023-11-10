862acc8fc560eca466ca68a24b548ce51a283aab
Sevara Ibragimova <s.ibragimova@newuu.uz>
2023-11-11 00:47:26 +0500

    m

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,20 +2,14 @@ float problemSolution1(float consumed_water) {
     float cost;
     if (consumed_water<=30){
         cost=13+consumed_water*0.4;
-        if (consumed_water<=50){
-            cost=13+30*0.4+(consumed_water-30)*0.12;
-            if (consumed_water<=60){
-                cost=13+30*0.4+20*0.12+(consumed_water-50)*1.4;
-            }
-
-
-        }
-
     }
+    else if (consumed_water<=50){
+        cost=13+30*0.4+(consumed_water-30)*0.12; }
+    else if (consumed_water<=60){
+                cost=13+30*0.4+20*0.12+(consumed_water-50)*1.4;}
     else if(consumed_water>60){
         cost=13+30*0.4+20*0.12+10*1.4+(consumed_water-60)*1.5;
     }
     // write your code here
-
     return cost;
 }
